50827c487680852ab69e609b585828f6c9154d4d
Nikhil <nikhilmaske2001@gmail.com>
2020-10-31 15:24:41 +0530

    Added multiple functions

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -6,18 +6,53 @@ def index(request):
 
 
 def analyze(request):
+    #Get the text
     djtext = request.GET.get('text','default')
+
+    #Check checkbox values
     removepunc = request.GET.get('removepunc','off')
-    print(removepunc)
-    print(djtext)
+    fullcaps = request.GET.get('fullcaps','off')
+    newline = request.GET.get('newline','off')
+    spaceremover = request.GET.get('spaceremover','off')
+    countcharacter = request.GET.get('countcharacter','off')
+
+    #checking which box is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
           if char not in punctuations:
               analyzed = analyzed + char
-
         params = {'purpose':'Removed Punctuation','analyzed_text':analyzed}
         return render(request, 'analyze.html', params)
+    
+    elif fullcaps == "on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif newline == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+        params = {'purpose': 'New line remover', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif spaceremover == "on":
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Extra space remover', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif countcharacter == "on":
+        count = len(djtext)
+        params = {'purpose': 'Count the number of character', 'analyzed_text': count}
+        return render(request, 'analyze.html', params)
+
     else:
-        return HttpResponse("Error")
+        return HttpResponse('Error')
